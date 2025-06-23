e8b0232577404df02032e186a07d8090db914215
Saar45 <nabysarr16@gmail.com>
2025-06-23 17:02:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dataventureTest',
+  webDir: 'www'
+};
+
+export default config;
